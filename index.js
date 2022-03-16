e44ef7cfaf0a6acfa761decfd2f586df064164a5
Italo Sergio <italosergio@graduate.org>
2022-03-16 16:09:18 -0300

    build: /index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}!`));
 
@@ -9,3 +10,14 @@ app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT
 app.get('/', (request, response) => {
   response.send();
 });
+
+const error = require('./middlewares/error');
+const userCreate = require('./controllers/userCreate');
+const authName = require('./middlewares/authName');
+const authEmail = require('./middlewares/authEmail');
+const authPass = require('./middlewares/authPass');
+const validateEmail = require('./middlewares/validateEmail');
+
+app.post('/user', authName, authEmail, authPass, validateEmail, userCreate);
+
+app.use(error);
