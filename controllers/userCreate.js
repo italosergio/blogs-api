d9c69cfb757c6f04415879e5cf293d7a3729de39
Italo Sergio <italosergio@graduate.org>
2022-03-17 00:16:10 -0300

    pass: requirement 1, build: create-user controller && token-generate service

---
+++ controllers/userCreate.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+const tokenGen = require('../services/tokenGen');
+
+module.exports = async (req, res, next) => {
+  try {
+    const user = req.body;
+    await User.create(user);
+    
+    const token = tokenGen(req);
+    return res.status(201).json({ token });
+  } catch (e) {
+    next(e);
+  }
+};
