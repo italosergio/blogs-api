ce4dc464f131d78cc86d56c6134a1b364883b9b3
Italo Sergio <italosergio@graduate.org>
2022-03-19 05:03:48 -0300

    refactor: requirement 5

--- controllers/categorieCreate.js
+++ controllers/categorieCreate.js
@@ -8,7 +8,12 @@ module.exports = async (req, res, _next) => {
     await User.findOne({ where: { email } });
 
     const { name } = req.body;
+    if (name === '') return res.status(400).json({ message: '"name" is not allowed to be empty' });
     if (!name) return res.status(400).json({ message: '"name" is required' });
+
+    const categorieWithThisName = await Categorie.findOne({ where: { name } });
+    if (categorieWithThisName) return res.status(404).json({ message: 'Category exists' });
+
     await Categorie.create({ name });
     const categorie = await Categorie.findOne({ where: { name } });
     return res.status(201).json(categorie);
