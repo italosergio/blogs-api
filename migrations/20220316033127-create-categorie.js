368a2471e61f35c5a4483f0479fba2caade0227e
Italo Sergio <italosergio@graduate.org>
2022-03-21 20:24:40 -0300

    fix: migrations - remove allowNull

--- migrations/20220316033127-create-categorie.js
+++ migrations/20220316033127-create-categorie.js
@@ -3,7 +3,6 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Categories', {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
