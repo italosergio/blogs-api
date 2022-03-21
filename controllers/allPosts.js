92a15a7181927fff6c11e2b88879e742ab4f2461
Italo Sergio <italosergio@graduate.org>
2022-03-21 17:34:00 -0300

    fix: requirement 8 - apply error middleware

--- controllers/allPosts.js
+++ controllers/allPosts.js
@@ -1,25 +1,30 @@
 const { BlogPost, PostsCategorie, Categorie } = require('../models');
 const findUser = require('../services/findUser');
 
-module.exports = async (req, res, next) => {
-  const allPosts = await BlogPost.findAll();
-  const postsCategories = await PostsCategorie.findAll();
-  const allCategories = await Categorie.findAll();
-
-  const getPostCategories = (id) => postsCategories
-    .reduce((acc, crr) => (crr.postId === id ? [...acc, crr.categoryId] : [...acc]), []);
+const getPostCategories = (id, array) => array
+  .reduce((acc, crr) => (crr.postId === id ? [...acc, crr.categoryId] : [...acc]), []);
   
-  const { user } = await findUser(req);
+module.exports = async (req, res, next) => {
+  try {
+    const allPosts = await BlogPost.findAll();
+    const postsCategories = await PostsCategorie.findAll();
+    const allCategories = await Categorie.findAll();
+    
+    const { user } = await findUser(req);
 
-  const posts = allPosts.map((post) => {
-    const postCategories = getPostCategories(post.dataValues.id);
-    const categories = allCategories.filter((cat) => postCategories.some((id) => id === cat.id));
+    const posts = allPosts.map((post) => {
+      const postCategories = getPostCategories(post.dataValues.id, postsCategories);
+      const categories = allCategories.filter((cat) => postCategories.some((id) => id === cat.id));
 
-    return {
-    ...post.dataValues,
-    user,
-    categories,
-    };
-  });
-  return res.status(200).json(posts);
+      return {
+        ...post.dataValues,
+        user,
+        categories,
+      };
+    });
+
+    return res.status(200).json(posts);
+  } catch (err) {
+    next(err);
+  }
 };
