2fa195eb727e58f47eb92a3bba08c6d58057de7a
Italo Sergio <italosergio@graduate.org>
2022-03-20 22:20:37 -0300

    fix: requirement 7 part 2

--- controllers/blogPost.js
+++ controllers/blogPost.js
@@ -8,7 +8,7 @@ module.exports = async (req, res, next) => {
   await BlogPost.create({ title, content });
 
   const posts = await BlogPost.findAll() || 0;
-  const postId = Object.keys(posts).length;
+  const postId = Object.keys(posts).length || 1;
 
   const token = req.headers.authorization;
   const { email } = tokenDecoder(token, process.env.JWT_SECRET);
