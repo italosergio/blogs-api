62e27527af64c24a797f5c2772fb00e6301a3bf7
Italo Sergio <italosergio@graduate.org>
2022-03-21 03:05:46 -0300

    update: blog post controller

--- controllers/blogPost.js
+++ controllers/blogPost.js
@@ -2,21 +2,39 @@ const { BlogPost } = require('../models');
 const { User } = require('../models');
 const tokenDecoder = require('../services/tokenDecoder');
 
-module.exports = async (req, res, next) => {
- try {
-  const { title, content } = req.body;
-  await BlogPost.create({ title, content });
-
+async function getPostId() {
   const posts = await BlogPost.findAll() || 0;
-  const postId = Object.keys(posts).length || 1;
+  const id = Object.keys(posts).length + 1 || 1;
+  return { id };
+}
 
+async function getUser(req) {
   const token = req.headers.authorization;
   const { email } = tokenDecoder(token, process.env.JWT_SECRET);
   const user = await User.findOne({ where: { email } });
+  const userId = user.dataValues.id;
+  return { userId };
+}
+
+function getDate() {
+  const now = new Date(Date.now()).toISOString();
+  const published = now;
+  return { published };
+}
+
+module.exports = async (req, res, next) => {
+  try {
+  const { id } = await getPostId();
+  const { userId } = await getUser(req);
+  const { title, content } = req.body;
+  getDate();
+  const { published } = getDate();
+
+  await BlogPost.create({ title, content, userId, published });
 
   const obj = {
-    id: postId,
-    userId: user.id,
+    id,
+    userId,
     title,
     content,
   };
