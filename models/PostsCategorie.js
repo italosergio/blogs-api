08f684c892bf64de5ab794937344b5da8c55c658
Italo Sergio <italosergio@graduate.org>
2022-03-21 17:13:11 -0300

    rerfactor: create porst-categories model

--- models/PostsCategorie.js
+++ models/PostsCategorie.js
@@ -1,5 +1,6 @@
 const PostsCategorie = (sequelize, DataTypes) => {
   const postsCategorie = sequelize.define('PostsCategorie', {
+    postId: DataTypes.INTEGER,
     categoryId: DataTypes.INTEGER,
   }, {
     timestamps: false,
