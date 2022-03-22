c1df72d62d6a0691c4ab2bb90b788795107df5e5
Italo Sergio <italosergio@graduate.org>
2022-03-22 00:50:52 -0300

    fix: bug require get user id (/user/:id)

--- controllers/userById.js
+++ controllers/userById.js
@@ -1,13 +1,9 @@
 const { User } = require('../models');
-const tokenDecoder = require('../services/tokenDecoder');
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.header('authorization');
-    const { email } = tokenDecoder(token);
-  
-    const { id } = req.params;
-    const user = await User.findOne({ where: { id, email } });
+    const { params: { id } } = req;
+    const user = await User.findOne({ where: { id } });
     if (!user) return res.status(404).json({ message: 'User does not exist' });
     return res.status(200).json(user);
   } catch (err) {
