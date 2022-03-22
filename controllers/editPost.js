a61a54935723873c5a88ddbc7ce877095f92761c
Italo Sergio <italosergio@graduate.org>
2022-03-22 13:10:01 -0300

    fix: edit post file

--- controllers/editPost.js
+++ controllers/editPost.js
@@ -3,8 +3,7 @@ const findAllPosts = require('../services/findAllPosts');
 module.exports = async (req, res, next) => {
   try {
     const { posts } = await findAllPosts(req, res, next);
-
-    res.send(posts[1]);
+    return res.send(posts[1]);
   } catch (err) {
     next(err);
   }
