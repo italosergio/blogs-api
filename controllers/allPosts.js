2ae0d182c15196ce9128741856f4eaf1ea6738f3
Italo Sergio <italosergio@graduate.org>
2022-03-22 23:41:20 -0300

    cleanup: simple remove at all posts controller

--- controllers/allPosts.js
+++ controllers/allPosts.js
@@ -2,7 +2,7 @@ const findAllPosts = require('../services/findAllPosts');
 
 module.exports = async (req, res, next) => {
   try {
-    const { posts } = await findAllPosts(req, res, next);
+    const { posts } = await findAllPosts(req);
 
     return res.status(200).json(posts);
   } catch (err) {
