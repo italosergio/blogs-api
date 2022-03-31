ccb3740c5f05ee0541f196f7713fbd8a8dc3b3e8
Italo Sergio <italosergio@graduate.org>
2022-03-31 01:52:56 -0300

    rename: controller postById to getPostById

--- controllers/getPostById.js
+++ controllers/getPostById.js
@@ -5,7 +5,6 @@ module.exports = async (req, res, next) => {
     const { posts } = await findAllPosts(req, res, next);
     const { id } = req.params;
     
-    if (!posts[id - 1]) return res.status(404).json({ message: 'Post does not exist' });
     return res.status(200).json(posts[id - 1]);
   } catch (err) {
     next(err);
